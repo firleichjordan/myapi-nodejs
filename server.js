6afcdd6636ef244bd08e5251f203b3c5c9980cb0
firleichjordan <jordanhombing.js@gmail.com>
2023-12-25 03:25:02 +0000

    add vercel

--- server.js
+++ server.js
@@ -5,3 +5,6 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 server.listen(port);
+
+
+// const jsonData = pm.response.json();if (jsonData.status) {    pm.environment.set("token", jsonData.token);    }
